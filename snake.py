5137d2124e1c98f14f67382e9830696c3f28a8ca
Clemence Pouyanne <clemence.pouyanne@etu.minesparis.psl.eu>
2022-11-09 15:49:18 +0100

    Eating the fruit

--- snake.py
+++ snake.py
@@ -16,6 +16,8 @@ screen = pygame.display.set_mode([20 * 30, 20 * 30])
 clock = pygame.time.Clock()
 
 while True:
+
+    #événement du clavier
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -32,15 +34,27 @@ while True:
                 direction = [1, 0]
             if event.key == pygame.K_LEFT and direction != [1, 0]:
                 direction = [-1, 0]
+    
+    #manger le fruit et grandir
+    if snake[0][0] == fruit[0]//20 and snake[0][1] == fruit[1]//20 :
+            fruit = [random.randint(0, 30) * 20, random.randint(0, 30) * 20, 20, 20]
+            newqueue = snake[-1].copy()
+            newqueue[0] += direction[0]
+            newqueue[1] += direction[1]
+            snake.append(newqueue)
+
+    #déplacement du serpent
     snake.pop()
     a = snake[0].copy()
     a[0] += direction[0]
     a[1] += direction[1]
     snake.insert(0, a)
     screen.fill(white)
+
+    #actualiser le dessin et l'écran
     pygame.draw.rect(screen, red, fruit)
     for x, y in snake:
         rect = [x * 20, y * 20, 20, 20]
         pygame.draw.rect(screen, black, rect)
         pygame.display.update()
-    clock.tick(5)
+    clock.tick(2)
